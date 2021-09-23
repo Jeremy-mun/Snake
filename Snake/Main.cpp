410b276d041030ca599ad8bec8b544abdc9fa035
jp6337 <jp6337@mun.ca>
2021-09-23 20:15:01 -0230

    displayed the score and head of the snake

--- Snake/Main.cpp
+++ Snake/Main.cpp
@@ -1,10 +1,24 @@
-#include <SFML/graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main()
+int main(int argc, char * argv[])
 {
-	sf::Window window(sf::VideoMode(800, 600), "Snake");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Snake");
 
+	sf::RectangleShape head(sf::Vector2f(25, 25));
+	head.setFillColor(sf::Color::Green);
+	head.setPosition(400, 400);
+
+	sf::Font myFont;
+
+	if (!myFont.loadFromFile("fonts/arial.ttf"))
+	{
+		std::cerr << "Could not load font!\n";
+		exit(-1);
+	}
+
+	sf::Text score("Score", myFont, 24);
+	score.setPosition(0, 0);
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -15,6 +29,11 @@ int main()
 				window.close();
 			}
 		}
+
+		window.clear();
+		window.draw(head);
+		window.draw(score);
+		window.display();
 	}
 
 	return 0;
